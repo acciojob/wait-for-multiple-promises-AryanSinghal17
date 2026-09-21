9e2296d3865b5e20cd0034ee935046f30838a2c9
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-21 20:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let tableBody = document.getElementById('output');
+tableBody.innerHTML = '<h2>Loading...</h2>';
+
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 1');
+    }, 1000);
+});
+
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 2');
+    }, 1500);
+});
+
+let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 3');
+    }, 2000);
+});
+
+Promise.all([p1, p2, p3])
+.then(data => {
+    console.log(data);
+    tableBody.innerHTML = data.join('<br>');
+})
+.catch(err => {
+    console.log(err);
+});
