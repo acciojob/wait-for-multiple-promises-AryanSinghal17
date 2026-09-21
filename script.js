e0e06388c687619ab03f9a28db9691aff5b3865e
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-21 20:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-let tableBody = document.getElementById('output');
-tableBody.innerHTML = '<h2>Loading...</h2>';
+let tableBody = document.getElementById("output");
 
-let p1 = new Promise((resolve, reject) => {
+tableBody.innerHTML = `
+<tr>
+    <td colspan="2">Loading...</td>
+</tr>
+`;
+
+let p1 = new Promise((resolve) => {
     setTimeout(() => {
-        resolve('Promise 1');
+        resolve(1);
     }, 1000);
 });
 
-let p2 = new Promise((resolve, reject) => {
+let p2 = new Promise((resolve) => {
     setTimeout(() => {
-        resolve('Promise 2');
+        resolve(1.5);
     }, 1500);
 });
 
-let p3 = new Promise((resolve, reject) => {
+let p3 = new Promise((resolve) => {
     setTimeout(() => {
-        resolve('Promise 3');
+        resolve(2);
     }, 2000);
 });
 
-Promise.all([p1, p2, p3])
-.then(data => {
-    console.log(data);
-    tableBody.innerHTML = data.join('<br>');
-})
-.catch(err => {
-    console.log(err);
-});
+Promise.all([p1, p2, p3]).then((times) => {
+
+    tableBody.innerHTML = "";
+
+    let total = 0;
+
+    times.forEach((time, index) => {
+        total += time;
+
+        tableBody.innerHTML += `
+        <tr>
+            <td>Promise ${index + 1}</td>
+            <td>${time}</td>
+        </tr>
+        `;
+    });
+
+    tableBody.innerHTML += `
+    <tr>
+        <td>Total</td>
+        <td>${total}</td>
+    </tr>
+    `;
+});``
